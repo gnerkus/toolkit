74113ba8f9d295235b1e7b2532dc349b1242a28b
ify <ifeanyioraelosi@gmail.com>
2019-11-01 17:28:58 +0100

    001: feat: init monorepo with lerna - install lerna and create monorepo - create packages directory - set up base react application for component library - - install react, react-dom, and babel presets - - install webpack and configure - - create simple components for testing app

---
+++ packages/ui-kit/README.md
@@ -0,0 +1,11 @@
+# `@toolkit/ui-kit`
+
+> TODO: description
+
+## Usage
+
+```
+const uiKit = require('@toolkit/ui-kit');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/ui-kit/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'index.js',
+  },
+  devServer: {
+    contentBase: './dist'
+  }
+};
