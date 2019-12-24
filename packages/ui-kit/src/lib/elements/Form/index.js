e1cacf9392e90bcd755c7edad069100ddaa97b6f
gnerkus <ifeanyioraelosi@gmail.com>
2019-12-24 13:04:48 +0100

    007: feat: define checkbox field

--- packages/ui-kit/src/lib/elements/Form/index.js
+++ packages/ui-kit/src/lib/elements/Form/index.js
@@ -2,10 +2,12 @@ import TextField from './TextField'
 import NumberField from './NumberField'
 import Select from './Select'
 import TextArea from './TextArea'
+import Checkbox from './Checkbox'
 
 export default {
   TextField,
   NumberField,
   Select,
-  TextArea
+  TextArea,
+  Checkbox
 }
